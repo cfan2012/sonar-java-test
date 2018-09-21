8df7163e984cb9c2a471dccc47063fb1b07c73e1
Liang <392425595@qq.com>
2018-09-21 11:38:41 +0800

    6

--- scripts/sonar_run.sh
+++ scripts/sonar_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=java -Dsonar.sources=java
+/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=java -Dsonar.sources=java -Dsonar.java.binaries=java/out
