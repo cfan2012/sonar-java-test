5dffa65c8f15154509cdf9fb0a5908e30d900810
Liang <392425595@qq.com>
2018-09-21 15:53:19 +0800

    add

--- scripts/sonar_run.sh
+++ scripts/sonar_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ant
-/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=java -Dsonar.sources=java -Dsonar.java.binaries=java/build/classes
+
+PROJECT_NAME=java
+
+/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=deskcheck -Dsonar.sources=${PROJECT_NAME} -Dsonar.java.binaries=${PROJECT_NAME}/build/classes
