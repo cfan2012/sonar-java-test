a03c5c252b2eafc81cc681278c167139a2f03669
cfan2012 <inon@vip.qq.com>
2018-09-30 11:35:17 +0800

    test34

--- scripts/sonar_run.sh
+++ scripts/sonar_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ant
 
-PROJECT_NAME=java
+PROJECT_NAME=xbtest
 
 /sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=xbtest -Dsonar.sources=${PROJECT_NAME} -Dsonar.java.binaries=${PROJECT_NAME}/build/classes
